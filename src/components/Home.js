68aeb160cb10353cf13b4eb93516574ff9354e9d
Marshall Zobel <dmzobel@gmail.com>
2018-04-30 14:16:50 -0400

    add: presentation commit

--- src/components/Home.js
+++ src/components/Home.js
@@ -5,10 +5,10 @@ import RaisedButton from 'material-ui/RaisedButton';
 export default class Home extends Component {
   render() {
     return (
-      <div className="center">
+      <div className="welcome center">
         <p className="title relative white-text fun-font">Welcome to Vintage</p>
         <p className="subtitle relative white-text">
-          Using climate data to predict the quality of red wine
+          Using climate data to predict red wine quality
         </p>
         <Link to="/rankings">
           <RaisedButton label="See the list" />
