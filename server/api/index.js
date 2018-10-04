ba702432ccec2828658a9dbbe17a07de23a72ca0
Marshall Zobel <dmzobel@gmail.com>
2018-10-04 16:43:35 -0400

    added query parameters to routes

--- server/api/index.js
+++ server/api/index.js
@@ -1,4 +1,42 @@
 const router = require('express').Router();
 module.exports = router;
+const { Vintage } = require('../db/models');
 
-router.use('/vintages', require('./vintages'));
+router.get('/vintages', async (req, res, next) => {
+  // all queries return results ordered by quality rating, in descending order
+  if (req.query.year && req.query.region) {
+    Vintage.findAll({
+      where: {
+        year: req.query.year,
+        region: req.query.region
+      },
+      order: [['quality', 'DESC']]
+    })
+      .then(queryResult => res.json(queryResult))
+      .catch(err => console.error(err));
+  } else if (req.query.year) {
+    Vintage.findAll({
+      where: {
+        year: req.query.year
+      },
+      order: [['quality', 'DESC']]
+    })
+      .then(queryResult => res.json(queryResult))
+      .catch(err => console.error(err));
+  } else if (req.query.region) {
+    Vintage.findAll({
+      where: {
+        region: req.query.region
+      },
+      order: [['quality', 'DESC']]
+    })
+      .then(queryResult => res.json(queryResult))
+      .catch(err => console.error(err));
+  } else {
+    Vintage.findAll({
+      order: [['quality', 'DESC']]
+    })
+      .then(allVintages => res.json(allVintages))
+      .catch(err => console.error(err));
+  }
+});
